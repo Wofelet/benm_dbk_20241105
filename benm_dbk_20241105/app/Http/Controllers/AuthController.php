0cff914dd6480059c50583ec20f0f191c73b212a
Briam Ernesto Navarro Menjivar <ia.briam320@ufg.edu.sv>
2024-11-14 18:32:26 -0600

    Avance

--- benm_dbk_20241105/app/Http/Controllers/AuthController.php
+++ benm_dbk_20241105/app/Http/Controllers/AuthController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers;
 use App\Models\User;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Hash;
+use App\Models\User;
 
 class AuthController extends Controller
 {
@@ -36,5 +37,46 @@ class AuthController extends Controller
                 "msg"=>"Usuario creado satisfactoriamente",
                 "count"=>1
             ]);
+    }
+    public function login(Request $request){
+        if(Auth::attempt($request->only("email","password"))){
+            return response()->json([
+                "success"=> false,
+                "errors"=>[
+                    "code"=>401,
+                    "msg"=> "No se reconocen las credenciales"
+            ],
+            "data"=>"",
+            "count"=> 0
+        ],401);
+        }
+        $user = User::where("email", $request->email)->firstOnFail();
+        $token =$user->createToken("auth_tokeb")->plainTextToken;
+
+        return response()->json([
+            "success"=> true,
+                "errors"=>[
+                    "code"=>200,
+                    "msg"=> ""
+            ],
+            "data"=>[
+                "acess_token"=> $token,
+                "token_type" =>"Bearer"
+            ],
+            "msg"=>"Ha iniciado sesion correctamente",
+            "count"=> 1
+        ],200);
+    }
+
+    public function me(Request $request){
+        return response()->json([
+            "success"=> true,
+            "errors"=>[
+                "code"=> 200,
+                "msg"=> ""
+            ],
+            "data"=>$request->user(),
+            "count"=> 1
+        ],200);
     }
 }
--- benm_dbk_20241105/routes/api.php
+++ benm_dbk_20241105/routes/api.php
@@ -6,3 +6,7 @@ use Illuminate\Support\Facades\Route;
 Route::get('/user', function (Request $request) {
     return $request->user();
 })->middleware('auth:sanctum');
+
+Route::post('/nuevousuario',[AuthController::class,"register"]);
+Route::post('/login',[AuthController::class,"login"]);
+Route::post('/usuario',[AuthController::class,"me"])->middleware('auth:sanctum');
